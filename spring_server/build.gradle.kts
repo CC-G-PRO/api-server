186aceabc014dd3108492dfb1037c044a5233d99
woooo <99981511+xhae000@users.noreply.github.com>
2025-05-13 17:15:38 +0900

    chore: add dependency for jpa, h2

--- spring_server/build.gradle.kts
+++ spring_server/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
+
     id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -25,6 +27,12 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // h2db (for dev process)
+    implementation("com.h2database:h2")
 }
 
 kotlin {
