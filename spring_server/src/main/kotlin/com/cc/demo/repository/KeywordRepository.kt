0ec88dcaed6db194aabfa852b6f5a44a54b5f082
woooo <99981511+xhae000@users.noreply.github.com>
2025-05-15 15:27:55 +0900

    remove: remove keyword table (entity, repo)

--- spring_server/src/main/kotlin/com/cc/demo/repository/KeywordRepository.kt
+++
@@ -1,6 +0,0 @@
-﻿package com.cc.demo.repository
-
-import com.cc.demo.entity.Keyword
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface KeywordRepository : JpaRepository<Keyword, Long>
