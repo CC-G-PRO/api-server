eccba1cbe094691f9123186edecd0e43da059223
woooo <99981511+xhae000@users.noreply.github.com>
2025-06-03 20:06:52 +0900

    feat: replace userId with jwt user

--- spring_server/src/main/kotlin/com/cc/demo/controller/ReportParserController.kt
+++ spring_server/src/main/kotlin/com/cc/demo/controller/ReportParserController.kt
@@ -2,11 +2,13 @@ package com.cc.demo.controller
 
 import com.cc.demo.repository.UserRepository
 import com.cc.demo.response.CommonResponse
+import com.cc.demo.security.UserPrincipal
 import com.cc.demo.service.PdfParsingService
 import com.cc.demo.service.UserTakenCouseService
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.text.PDFTextStripper
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -43,15 +45,17 @@ class ReportParserController (
     }
 
     @PostMapping("/upload", consumes = ["multipart/form-data"])
-    fun handlePdfUpload(@RequestParam("file") file: MultipartFile?): ResponseEntity<Any> {
+    fun handlePdfUpload(
+        @RequestParam("file") file: MultipartFile?,
+        @AuthenticationPrincipal user: UserPrincipal,
+    ): ResponseEntity<Any> {
         if (file == null || file.isEmpty) {
             return ResponseEntity.badRequest().body("파일이 없습니다.")
         }
 
         return try {
-            //임의로 userid = 1에 저장. test 끝나면 jwt 로직으로 고칠것.
-            // 테스트용으로 userId 하드코딩. 나중에 JWT에서 user 받아올 예정
-            val response = pdfParsingService.processReportUpload(file, 1) //userId 말고 user 넘기게 고쳐야함.
+
+            val response = pdfParsingService.processReportUpload(file, user.id) //userId 말고 user 넘기게 고쳐야함.
             ResponseEntity.ok(response)
 
         } catch (e: Exception) {
